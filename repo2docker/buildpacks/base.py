864bb3800df8d7cf2a0c393634439c129b039306
slejdops <slejdops@gmail.com>
2019-01-20 10:27:57 +0000

    Update base.py

--- repo2docker/buildpacks/base.py
+++ repo2docker/buildpacks/base.py
@@ -38,10 +38,10 @@ ARG NB_UID
 ENV USER ${NB_USER}
 ENV HOME /home/${NB_USER}
 
-RUN adduser --disabled-password \
-    --gecos "Default user" \
-    --uid ${NB_UID} \
-    ${NB_USER}
+#RUN adduser --disabled-password \
+#    --gecos "Default user" \
+#    --uid ${NB_UID} \
+#    ${NB_USER}
 
 RUN wget --quiet -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key |  apt-key add - && \
     DISTRO="bionic" && \
